02510d6929cf1038a5be10b6de2254ff269c91a1
Josh Burgess <burgessj247@gmail.com>
2021-07-27 17:05:12 -0400

    Adjustments for deploy

--- client/src/pages/RSVPD.jsx
+++ client/src/pages/RSVPD.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState, useRef } from 'react';
